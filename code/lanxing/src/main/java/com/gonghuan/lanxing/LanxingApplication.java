0312989b5c027835ebd9a14e261a5e9f31e592b5
SJuner <312189329@qq.com>
2017-05-18 22:55:39 +0800

    lanxing Spring boot project start

---
+++ code/lanxing/src/main/java/com/gonghuan/lanxing/LanxingApplication.java
@@ -0,0 +1,12 @@
+package com.gonghuan.lanxing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanxingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanxingApplication.class, args);
+	}
+}
---
+++ code/lanxing/src/test/java/com/gonghuan/lanxing/LanxingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gonghuan.lanxing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LanxingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
